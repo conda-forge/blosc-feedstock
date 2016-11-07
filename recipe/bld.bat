599ab23a6ff6e31a227b66ceea3e23d2a5a501ab
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-11-07 12:56:23 -0500

    recipe: Add a Windows build script.

---
+++ recipe/bld.bat
@@ -0,0 +1,11 @@
+mkdir build
+cd build
+
+cmake -G "NMake Makefiles" ^
+         -DCMAKE_BUILD_TYPE=Release ^
+         -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+         ..
+
+cmake --build .
+ctest
+cmake --build . --target install
