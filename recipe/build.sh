350a1853c4682cc6fba691d044e10c6d59bd9aab
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-04 03:05:00 -0500

    Clean up Unix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,15 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -G "Unix Makefiles" \
+      -DCMAKE_BUILD_TYPE="Release" \
+      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+      -DBUILD_STATIC=1 \
+      -DBUILD_SHARED=1 \
+      -DBUILD_TESTS=1 \
+      -DBUILD_BENCHMARKS=0 \
+      "${SRC_DIR}"
 
 cmake --build .
 ctest
