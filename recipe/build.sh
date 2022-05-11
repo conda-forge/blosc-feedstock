449e30d0f3c1ce8670821e14a417a5a628cac0a3
John Kirkham <jakirkham@gmail.com>
2022-05-10 19:44:03 -0700

    Disable static libraries
    Policy in conda-forge is not to include static libraries. These can be
    broken out in a separate split package on request.
    
    https://github.com/conda-forge/cfep/blob/main/cfep-18.md

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ cmake ${CMAKE_ARGS} -G "Unix Makefiles" \
       -DCMAKE_BUILD_TYPE="Release" \
       -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
       -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
-      -DBUILD_STATIC=1 \
+      -DBUILD_STATIC=0 \
       -DBUILD_SHARED=1 \
       -DBUILD_TESTS=1 \
       -DBUILD_BENCHMARKS=0 \
